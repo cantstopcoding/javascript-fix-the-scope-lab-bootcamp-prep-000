5d41ed9c3d9c1a530b1d50fe2511676b6a92f868
Jonathan Koss <jonathan_koss@yahoo.com>
2018-10-01 18:47:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ function yourAnimal() {
     return animal;
 }
 
+function add2(n) {
+  return n + 2;
+}
+
+/*describe('add2(n)', function() {
+    it('adds two to n', function() {
+      const n = Math.floor(Math.random() * 1000)
+      expect(window.add2(n)).toEqual(n + 2)
+    })
+  }) */
 
 
 var funkyFunction = function() {
